fe1918e08cd61cc368010f03882ba826dafb5b21
v4lkyr <valkyr23@gmail.com>
2020-11-28 12:19:39 +0800

    skip ramdisk modifications

--- anykernel.sh
+++ anykernel.sh
@@ -34,9 +34,10 @@ set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 ## AnyKernel install
-dump_boot;
-patch_cmdline sched_enable_hmp sched_enable_hmp=0;
-write_boot;
+split_boot;
+
+flash_boot;
+flash_dtbo;
 
 ## end install
 
